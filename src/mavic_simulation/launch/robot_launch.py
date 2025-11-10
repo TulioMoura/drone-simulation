57447cff45afcb082d7ca134d4e41c107960004a
Túlio <90994955+TulioMoura@users.noreply.github.com>
2025-11-10 11:56:05 -0300

    Update robot_launch.py

--- src/mavic_simulation/launch/robot_launch.py
+++ src/mavic_simulation/launch/robot_launch.py
@@ -85,18 +85,13 @@ def generate_wbt_file(drones):
         print("Models Written to world file")
         
         # Verifica se já existe um arquivo updated_world.wbt (se existir dá erro). Se sim, apaga ele.
-        if os.path.exists('install/mavic_simulation/share/mavic_simulation/worlds/updated_world.wbt'):
-            os.remove('install/mavic_simulation/share/mavic_simulation/worlds/updated_world.wbt')
+        dest = 'install/mavic_simulation/share/mavic_simulation/worlds/updated_world.wbt'
+        if os.path.exists(dest):
+            os.remove(dest)
 
-        # Destino do novo arquivo de mundo que será usado pela simulação
-        destination_dir = 'install/mavic_simulation/share/mavic_simulation/worlds/'
-        
-        # Save the updated WBT content to a new file
-        with open('updated_world.wbt', 'w') as output_file:
-            output_file.write(wbt_content)
+        with open(dest, 'w') as f:
+            f.write(wbt_content)
 
-        # Move novo arquivo de mundo para a pasta onde ele será lido posteriormente
-        shutil.move('updated_world.wbt', destination_dir)
 
 
 
